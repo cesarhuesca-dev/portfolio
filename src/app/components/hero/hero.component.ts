63bf53025b37196f738e41942dc79cf861d2278a
cesarhuesca.dev <cesarhuesca.dev@gmail.com>
2026-05-02 19:51:51 +0200

    Estilos hero component

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -1,6 +1,9 @@
 import { NgOptimizedImage } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject } from '@angular/core';
 import { TranslatePipe } from '@ngx-translate/core';
+import { DownloadService } from '@services/download.service';
+import { LanguageService } from '@services/language.service';
+import { NavigateService, PortfolioSections } from '@services/navigate.service';
 import { AnimateOnScrollModule } from 'primeng/animateonscroll';
 
 @Component({
@@ -9,4 +12,20 @@ import { AnimateOnScrollModule } from 'primeng/animateonscroll';
   templateUrl: './hero.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HeroComponent {}
+export class HeroComponent {
+  private readonly navigateService = inject(NavigateService);
+  private readonly downloadService = inject(DownloadService);
+  private readonly languageService = inject(LanguageService);
+
+  readonly userLang = computed(() => this.languageService.lang);
+
+  readonly portfolioSections = PortfolioSections;
+
+  goToSection(sectionId: PortfolioSections) {
+    this.navigateService.goToSection(PortfolioSections[sectionId]);
+  }
+
+  downloadCv() {
+    this.downloadService.downloadCV(this.userLang());
+  }
+}
